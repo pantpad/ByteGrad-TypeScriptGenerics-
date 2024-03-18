4a1e6edcb0ae2497d7b6c3e9925c4fda12218a9a
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-18 10:40:09 +0100

    readme changes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <a href="https://www.youtube.com/watch?v=ymSRTXT-iK4" target="_blank">
+        ByteGrad
+      </a>
+    </>
+  );
+}
+
+export default App;
