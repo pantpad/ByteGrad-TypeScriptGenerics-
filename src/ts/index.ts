59dc753e16b242c9c05c5bfe0a54d313c1aa6109
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-18 10:43:18 +0100

    identity function returning array

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,9 @@
-console.log("ciao a tutti");
+//With generics you codify a relationship between your input and return value.
+
+function convertToArray<T>(input: T): T[] {
+  return [input];
+}
+
+console.log(convertToArray(1));
+console.log(convertToArray("str"));
+console.log(convertToArray({ name: "object" }));
