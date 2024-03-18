1ad3834116514c43e9a59360102be66b93d63064
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-18 10:50:25 +0100

    Add generic function w/2 generic inputs

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
 //With generics you codify a relationship between your input and return value.
-
+console.log(
+  "With generics you codify a relationship between your input and return value."
+);
 function convertToArray<T>(input: T): T[] {
   return [input];
 }
@@ -7,3 +9,20 @@ function convertToArray<T>(input: T): T[] {
 console.log(convertToArray(1));
 console.log(convertToArray("str"));
 console.log(convertToArray({ name: "object" }));
+
+//Function that given an array of generics value returns the index of the item specified
+console.log(
+  "Function that given an array of generics value returns the index of the item specified"
+);
+
+const arr = [11, 33, 88];
+
+function getItemIndexFromArr<T>(arr: T[], value: T) {
+  return arr.findIndex((item) => item === value);
+}
+
+const getItemIndexFromArrArrow = <T>(arr: T[], value: T) =>
+  arr.findIndex((item) => item === value);
+
+console.log(getItemIndexFromArr(arr, 88));
+console.log(getItemIndexFromArrArrow(arr, 88));
